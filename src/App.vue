1b6d98d5ced82a28fc96eda05bd70de07161cc6a
pcunha29 <pcunhadev@gmail.com>
2024-10-22 01:18:39 +0100

    setup project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="flex items-center border-b-2 pb-6">
+      <img src="./assets/logo.svg" alt="unitedlogo" class="w-60 mr-7" />
+      <nav class="w-36 flex justify-between">
+        <RouterLink to="/">Details Page</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
